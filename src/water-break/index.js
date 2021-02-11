e42e5c217ea160d6379c312fe2d24d9574740a9a
Alok Raj <alokr417@gmail.com>
2021-02-12 00:35:00 +0530

    style: 💄 notification view update
    this commit include commit related to notification view update.

--- src/water-break/index.js
+++ src/water-break/index.js
@@ -5,19 +5,19 @@ let alreadyRunning = false
 
 const start = (config) => {
   if (alreadyRunning) {
-    window.showInformationMessage('water break is already running')
+    window.showInformationMessage('water break is already running', 'Ok')
     return
   }
 
   alreadyRunning = true
   timer = setInterval(() => {
-    window.showInformationMessage('It\'s time to drink Water.')
+    window.showInformationMessage('It\'s time to drink Water. 👀💧', 'alright!')
   }, 1000 * 60 * config.minutesTillWaterBreak)
 }
 
 const stop = () => {
   if (!alreadyRunning) {
-    window.showInformationMessage('water break is already stopped')
+    window.showInformationMessage('water break is already stopped', 'Ok')
     return
   }
 
